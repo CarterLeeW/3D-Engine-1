d9d4886ccdfeb2fa9266448239dcfd66800d27b2
CarterLeeW <103856985+CarterLeeW@users.noreply.github.com>
2023-12-05 19:52:06 -0500

    prepare shaders and things for rendering a cube

--- src/Cube.cpp
+++ src/Cube.cpp
@@ -74,7 +74,31 @@ namespace prim3d {
 
     void Cube::draw(Shader& shader) const
     {
+        // bind appropriate textures
+        unsigned int diffuseNr = 1;
+        unsigned int specularNr = 1;
 
+        for (unsigned int i = 0; i < textures.size(); i++)
+        {
+            glActiveTexture(GL_TEXTURE0 + i); // active proper texture unit before binding
+            // retrieve texture number (the N in diffuse_textureN)
+            std::string number;
+            std::string name = textures[i].type;
+            if (name == "texture_diffuse")
+                number = std::to_string(diffuseNr++);
+            else if (name == "texture_specular")
+                number = std::to_string(specularNr++); // transfer unsigned int to string
+
+            shader.setInt(("material." + name + number).c_str(), i);
+            glBindTexture(GL_TEXTURE_2D, textures[i].id);
+        }
+
+        // draw mesh
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        glBindVertexArray(0);
+
+        glActiveTexture(GL_TEXTURE0);
     }
 
 } // namespace prim3d
--- src/Game.cpp
+++ src/Game.cpp
@@ -7,6 +7,8 @@ void Game::init(const char* title, int width, int height, bool fullscreen)
 {
 	renderer = new Renderer(title, width, height);
 	// FIXME load scene from scene.cpp
+
+
 	isRunning = true;
 }
 
@@ -34,6 +36,35 @@ void Game::render()
 	// loop and render meshes
 	//
 	//
+    ourShader.use();
+    ourShader.setVec3("viewPos", camera.Position);
+    ourShader.setFloat("material.shininess", 32.0f);
+    // spotLight
+    ourShader.setVec3("light.position", camera.Position);
+    ourShader.setVec3("light.direction", camera.Front);
+    ourShader.setVec3("light.ambient", 0.0f, 0.0f, 0.0f);
+    ourShader.setVec3("light.diffuse", 1.0f, 1.0f, 1.0f);
+    ourShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
+    ourShader.setFloat("light.constant", 1.0f);
+    ourShader.setFloat("light.linear", 0.09f);
+    ourShader.setFloat("light.quadratic", 0.032f);
+    ourShader.setFloat("light.cutOff", glm::cos(glm::radians(12.5f)));
+    ourShader.setFloat("light.outerCutOff", glm::cos(glm::radians(15.0f)));
+
+    // view/projection transformations
+    glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+    glm::mat4 view = camera.GetViewMatrix();
+    ourShader.setMat4("projection", projection);
+    ourShader.setMat4("view", view);
+
+    // render the loaded model
+    glm::mat4 model = glm::mat4(1.0f);
+    model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f)); // translate it down so it's at the center of the scene
+    model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));	// it's a bit too big for our scene, so scale it down
+    ourShader.setMat4("model", model);
+    ourShader.setMat3("modelMatNormals", glm::mat3(transpose(inverse(model))));
+    ourModel.Draw(ourShader);
+
 
 	glfwSwapBuffers(renderer->window);
 	glfwPollEvents();
