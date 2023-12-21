6302b8532b1fb61dd7c28842f36e0caddd162273
CarterLeeW <103856985+CarterLeeW@users.noreply.github.com>
2023-12-21 17:43:49 -0500

    translations struct and getter/setters added to primitive3d.h

--- src/Primitive3D.h
+++ src/Primitive3D.h
@@ -5,77 +5,93 @@
 #include <stb_image/stb_image.h>
 #include <vector>
 #include <string>
+#include <glm/glm.hpp>
+#include <glm/gtx/transform.hpp>
 
 
 
+struct Transformations {
+    glm::mat4 scale = glm::mat4(1.0f);
+    glm::mat4 rotation = glm::mat4(1.0f);
+    glm::mat4 translation = glm::mat4(1.0f);
+    glm::mat4 model = glm::mat4(1.0f);
+};
 
+struct Vertex {
+	glm::vec3 Position;
+	glm::vec3 Normal;
+	glm::vec2 TexCoords;
+};
 
-	struct Vertex {
-		glm::vec3 Position;
-		glm::vec3 Normal;
-		glm::vec2 TexCoords;
-	};
-
-	struct Texture {
-		unsigned int id;
-		std::string type;
-	};
+struct Texture {
+	unsigned int id;
+	std::string type;
+};
     
 
-	class Primitive3D
-	{
-	public:
-		
-		Primitive3D();
-		virtual ~Primitive3D();
-		virtual void draw(Shader& shader) = 0;
-        void setTexture(std::string type, const char* path);
-		virtual void deleteMesh();
+class Primitive3D
+{
+public:
 
-		unsigned int loadTexture(char const* path)
+    Primitive3D();
+    virtual ~Primitive3D();
+    virtual void draw(Shader& shader) = 0;
+    void setTexture(std::string type, const char* path);
+    virtual void deleteMesh();
+
+    unsigned int loadTexture(char const* path)
+    {
+        unsigned int textureID;
+        glGenTextures(1, &textureID);
+
+        int width, height, nrComponents;
+        unsigned char* data = stbi_load(path, &width, &height, &nrComponents, 0);
+        if (data)
         {
-            unsigned int textureID;
-            glGenTextures(1, &textureID);
+            GLenum format;
+            if (nrComponents == 1)
+                format = GL_RED;
+            else if (nrComponents == 3)
+                format = GL_RGB;
+            else if (nrComponents == 4)
+                format = GL_RGBA;
 
-            int width, height, nrComponents;
-            unsigned char* data = stbi_load(path, &width, &height, &nrComponents, 0);
-            if (data)
-            {
-                GLenum format;
-                if (nrComponents == 1)
-                    format = GL_RED;
-                else if (nrComponents == 3)
-                    format = GL_RGB;
-                else if (nrComponents == 4)
-                    format = GL_RGBA;
+            glBindTexture(GL_TEXTURE_2D, textureID);
+            glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+            glGenerateMipmap(GL_TEXTURE_2D);
 
-                glBindTexture(GL_TEXTURE_2D, textureID);
-                glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
-                glGenerateMipmap(GL_TEXTURE_2D);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-                stbi_image_free(data);
-            }
-            else
-            {
-                std::cout << "Texture failed to load at path: " << path << std::endl;
-                stbi_image_free(data);
-            }
-
-            return textureID;
+            stbi_image_free(data);
+        }
+        else
+        {
+            std::cout << "Texture failed to load at path: " << path << std::endl;
+            stbi_image_free(data);
         }
 
-	protected:
-		unsigned int VAO, VBO;
-        std::vector<Texture> textures;
-		bool isInitialized = false;
+        return textureID;
+    }
+    glm::mat4 getScale() { return transform.scale; }
+    glm::mat4 getRoation() { return transform.rotation; }
+    glm::mat4 getTranslation() { return transform.translation; }
+    glm::mat4 getModel() { return transform.model; }
+    void setScale(glm::vec3 xyz) { transform.scale = glm::scale(xyz); }
+    void setRotation(float degrees, glm::vec3 xyz) { transform.rotation = glm::rotate(glm::radians(degrees), xyz); }
+    void setTranslation(glm::vec3 xyz) { transform.translation = glm::translate(xyz); }
+    void updateModel() { transform.model = transform.translation * transform.rotation * transform.scale; }
+
+protected:
+	unsigned int VAO, VBO;
+    std::vector<Texture> textures;
+	bool isInitialized = false;
+    Transformations transform;
 
         
-		virtual void initializeMesh() {}; // only for some primitives
-		virtual void initializeData() {};
-	};
+	virtual void initializeMesh() {}; // only for some primitives
+	virtual void initializeData() {};
+};
 
