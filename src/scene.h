afc81ffcde1a6172ada715d09fc8e55139e6f169
CarterLeeW <103856985+CarterLeeW@users.noreply.github.com>
2023-12-21 18:06:28 -0500

    move cube model out of render loop and add rotation in render loop

--- src/scene.h
+++ src/scene.h
@@ -26,6 +26,11 @@ namespace scene {
 
         // first cube object
         cube1 = new Cube(true, false);
+        // set initial world space
+        cube1->setScale(glm::vec3(2.0f));
+        cube1->setRotation(45.0f, glm::vec3(1.0f));
+        cube1->setTranslation(glm::vec3(0.0f, 0.0f, -4.0f));
+        cube1->updateModel();
         cube1->setTexture("texture_diffuse", RESOURCES_PATH "textures/container2.png");
         cube2 = new Cube(true, false);
         cube2->setTexture("texture_diffuse", RESOURCES_PATH "textures/container2.png");
@@ -43,34 +48,12 @@ namespace scene {
         shader->setMat4("view", view);
 
         // world transformation
-        // FIXME: make these variables class attributes eventually
-        // static objects do not need to update in the render loop
-        // template:
-        /*
-        scale = glm::scale(glm::vec3(0.75f, 0.2f, 0.25f));
-        rotation = glm::rotate(glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-        translation = glm::translate(glm::vec3(-2.2f, 0.4f, 0.0f));
-        model = translation * rotation * scale;
-        */
-        glm::mat4 scale = glm::mat4(1.0f);
-        glm::mat4 rotation = glm::mat4(1.0f);
-        glm::mat4 translation = glm::mat4(1.0f);
-        glm::mat4 model = glm::mat4(1.0f);
         // cube1
-        scale = glm::scale(glm::vec3(3.0));
-        rotation = glm::rotate(glm::radians(45.0f), glm::vec3(1.0f));
-        translation = glm::translate(glm::vec3(0.0f, 0.0f, -4.0f));
-        model = translation * rotation * scale;
-        shader->setMat4("model", model);
+        cube1->setRotation((float)glfwGetTime() *40.0f, glm::vec3(0.0f, 1.0f, 0.0f));
+        cube1->updateModel();
+        shader->setMat4("model", cube1->getModel());
         cube1->draw(*shader);
-        // cube2
-        scale = glm::scale(glm::vec3(2.0f, 1.0f, 1.0f));
-        rotation = glm::rotate(glm::radians(0.0f), glm::vec3(1.0f));
-        translation = glm::translate(glm::vec3(3.0f, 2.0f, -2.0f));
-        model = translation * rotation * scale;
-        shader->setMat4("model", model);
-        cube2->draw(*shader);
-
+        
         
 	}
 
